9f69c2343e2af2929a9a950d65efdff08c08eb1f
Dibyaprakash25 <dibyaprakshprusty25@gmail.com>
2021-08-02 19:06:54 +0530

    deployed

---
+++ build/precache-manifest.579b3dc1ca3fc7b2339c5477cb51a5c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea19757e09116a778638dac4c84d634c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "599f727a25224ca4cb61",
+    "url": "/keeper-app/static/js/2.14ebd957.chunk.js"
+  },
+  {
+    "revision": "37ffd1e7af31a36df7c3",
+    "url": "/keeper-app/static/js/main.bce53333.chunk.js"
+  },
+  {
+    "revision": "ed9243ad83c12b8fd98c",
+    "url": "/keeper-app/static/js/runtime-main.604284a8.js"
+  }
+]);
